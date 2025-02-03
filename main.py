c0cda5de69c840a005283f5fe278355fb7404bd2
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-03 18:31:26 +0200

    1 task add

---
+++ main.py
@@ -0,0 +1,19 @@
+import datetime
+# Example
+dt_res = datetime.datetime.today()
+print(dt_res)
+print('---------------------------------------')
+# Example
+print(dt_res.year)
+print('---------------------------------------')
+# Example
+print(dt_res.month)
+print('---------------------------------------')
+# Example
+print(dt_res.day)
+print('---------------------------------------')
+# Example
+print(dt_res.hour)
+print('---------------------------------------')
+# Example
+print(dt_res.minute)
---
+++ tasks.py
@@ -0,0 +1,10 @@
+# 1 Task
+import datetime
+dt_res = datetime.datetime.today()
+print(dt_res.year)
+print(dt_res.month)
+print(dt_res.day)
+print(dt_res.hour)
+print(dt_res.minute)
+print(dt_res.second)
+print(f'Dabar yra {dt_res.hour}:{dt_res.minute}, {dt_res.day}-{dt_res.month}-{dt_res.year}')
