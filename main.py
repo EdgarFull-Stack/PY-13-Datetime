e255cf8873c22147ee615a5cfcce2cfc775d4f3e
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-03 19:21:51 +0200

    2-3 tasks add

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import datetime
 # Example
 dt_res = datetime.datetime.today()
 print(dt_res)
-print('---------------------------------------')
-# Example
 print(dt_res.year)
-print('---------------------------------------')
-# Example
 print(dt_res.month)
-print('---------------------------------------')
-# Example
 print(dt_res.day)
-print('---------------------------------------')
-# Example
 print(dt_res.hour)
+print(dt_res.minute)
 print('---------------------------------------')
 # Example
-print(dt_res.minute)
+dt_res = datetime.datetime(2011, 12, 31, 23, 59, 59)
+print(dt_res)
+today_dt = datetime.datetime.today()
+if dt_res < today_dt:
+    print('Yes')
+print('---------------------------------------')
+#Example
+time_from_2000 = datetime.datetime.today() - datetime.datetime(2000, 1, 1)
+print(time_from_2000)
--- tasks.py
+++ tasks.py
@@ -8,3 +8,13 @@ print(dt_res.hour)
 print(dt_res.minute)
 print(dt_res.second)
 print(f'Dabar yra {dt_res.hour}:{dt_res.minute}, {dt_res.day}-{dt_res.month}-{dt_res.year}')
+print('--------------------------------------------')
+# 2 Task
+print(datetime.datetime(1995, 7, 14, 15 ,30))
+datetime_obj = datetime.datetime(1995, 7, 14)
+print(datetime_obj.date())
+print('--------------------------------------------')
+# 3 Task
+time_from_2000 = datetime.datetime.today() - datetime.datetime(2000, 1, 1)
+print(f'Praejo {time_from_2000.days} dienu nuo 2000-01-01')
+print('--------------------------------------------')
